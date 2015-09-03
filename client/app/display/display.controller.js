47d6dac2215c3389b92e773ba794517fb62c3152
Robert Lin <Rlin415@gmail.com>
2015-09-03 16:05:51 -0700

    added bar graph

--- client/app/display/display.controller.js
+++ client/app/display/display.controller.js
@@ -8,16 +8,48 @@ angular.module('voteAppApp')
     $scope.formData = {};
     $scope.getCurrentUser = Auth.getCurrentUser();
 
+    /***************** Configure Chart ****************/
+    var ctx;
+    var myBarChart;
+    var data = {
+      labels: [],
+      datasets: [
+          {
+              label: "My First dataset",
+              fillColor: "rgba(220,220,220,0.5)",
+              strokeColor: "rgba(220,220,220,0.8)",
+              highlightFill: "rgba(220,220,220,0.75)",
+              highlightStroke: "rgba(220,220,220,1)",
+              data: []
+          }
+        ]
+    };
+    var runChart = function() {
+      ctx = document.getElementById("myBarChart").getContext("2d");
+      myBarChart = new Chart(ctx).Bar(data);
+    };
+
+
     $http.get('/api/polls/' + pollid).success(function(poll) {
       $scope.poll = poll;
+      $scope.poll.choices.forEach(function(pollChoices) {
+      data.labels.push(pollChoices.name);
+      data.datasets[0].data.push(pollChoices.votes);
+      });
+      runChart();
     });
 
     $scope.vote = function() {
       if ($scope.poll.voted.indexOf($scope.getCurrentUser._id == -1 && $scope.formData.selection != undefined && $scope.isLoggedIn)) {
         $scope.poll.voted.push($scope.getCurrentUser._id);
         $scope.poll.choices[$scope.formData.selection].votes++;
+        //update bar chart here
+        myBarChart.datasets[0].bars[$scope.formData.selection].value = $scope.poll.choices[$scope.formData.selection].votes;
+        myBarChart.update();
+
         $http.patch('/api/polls/' + pollid, {choices: [{name: $scope.poll.choices[$scope.formData.selection].name, votes: $scope.poll.choices[$scope.formData.selection].votes}]}).then(function(poll) {
         });
       };
     };
+
   });
